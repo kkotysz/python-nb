2b74a625425764b628aa3cfdb3d379b959cd6dad
kkotysz <k.kotysz@gmail.com>
2025-04-03 13:22:28 +0200

    3 notebook

---
+++ understand_name_main.py
@@ -0,0 +1,16 @@
+def step1():
+    print(__name__)
+    print("Executing step1...")
+
+def step2():
+    print(__name__)
+    print("Executing step2...")
+
+def step3():
+    print(__name__)
+    print("Executing step3...")
+
+if __name__ == "__main__":
+    step1()
+    step2()
+    step3()
